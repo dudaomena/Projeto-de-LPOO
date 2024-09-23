8219a34bff3c1edbb646c85e7af49b510bb61eb4
Maria Eduarda <Maria Eduarda@192.168.0.105>
2024-09-23 10:09:25 -0300

    Janela inicial criada

---
+++ JogoArcana/src/jogo/Arcana.java
@@ -0,0 +1,23 @@
+package jogo;
+
+import javax.swing.JFrame;
+import jogo.Modelo.Base;
+
+public class Arcana extends JFrame {
+
+	public Arcana() {
+		add(new Base());
+
+		setTitle("Crônica de Arcana");
+		setSize(1024,728);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		setVisible(true);
+	}
+	
+	public static void main (String []args) {
+		new Arcana();
+	}
+}
+
---
+++ JogoArcana/src/jogo/Modelo/Base.java
@@ -0,0 +1,23 @@
+package jogo.Modelo;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Base extends JPanel {
+
+	private Image fundo;
+	
+	public Base() {
+		ImageIcon referencia = new ImageIcon("ImagensArcana\\Fundo1.jpg");
+		fundo = referencia.getImage();
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D graficos = (Graphics2D) g;
+		graficos.drawImage(fundo, 0, 0, null);
+		g.dispose();
+	}
+}
